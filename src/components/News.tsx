2d3007d6dcfb1cb52a6c1431004ddb0863534b59
Michał Krul <michal@krul.pl>
2020-12-28 19:38:01 +0100

    WIP: about page

---
+++ src/components/News.tsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import styled from 'styled-components'
+import { PageSectionHeader, SectionWrapper } from './shared'
+interface SpecialityList {
+  invert?: boolean
+}
+
+const SpecialityList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  margin: 0 0 50px 0;
+  padding: 0;
+  width: 100%;
+  list-style: none;
+  & > * {
+    color: ${(props: SpecialityList) => (props.invert ? 'white' : 'gray')};
+  }
+`
+const SpecialityListItem = styled.li`
+  margin-left: 0;
+  padding: 0;
+  font: 100 1.2em 'Lato';
+  line-height: 2em;
+  text-align: left;
+  & > span {
+    font: 400 1em 'Lato';
+  }
+`
+const News = () => {
+  return (
+    <SectionWrapper id={'news'}>
+      <PageSectionHeader>Aktualności</PageSectionHeader>
+      <SpecialityList>
+        <SpecialityListItem>
+          <span>21.09.2020</span> – zapraszamy do obejrzenia nowej wersji naszej
+          strony internetowej.
+        </SpecialityListItem>
+        <SpecialityListItem>
+          <span>27.07.2020</span> – Wdrożyliśmy nowy system do kontroli obiegu
+          odpadów pochodzących z produkcji.
+        </SpecialityListItem>
+        <SpecialityListItem>
+          <span>15.07.2020</span> – Witamy w naszych szeregach nowego
+          specjalistę ds. sprzedaży. Życzymy Tomkowi dobrych wyników i miłej
+          pracy.
+        </SpecialityListItem>
+        <SpecialityListItem>
+          <span>01.07.2020</span> – Witamy w naszym gronie nowych magazynierów –
+          Arek i Paweł – życzymy sukcesów w pracy.
+        </SpecialityListItem>
+        <SpecialityListItem>
+          <span>30.06.2020</span> – Zakończyliśmy wdrożenie nowego systemu
+          produkcyjnego w ramach systemu ERP, uwzględniającego moduł MES.
+        </SpecialityListItem>
+        <SpecialityListItem>
+          <span>15.05.2020</span> – Wdrożyliśmy nowy system magazynowy w ramach
+          systemu ERP. Zakończyliśmy optymalizację powierzchni magazynowej.
+        </SpecialityListItem>
+        <SpecialityListItem>
+          <span>30.04.2020</span> – Rekrutacja wewnętrzna wyłoniła nowego
+          kierownika Magazynu – Hubert, życzymy samych sukcesów w pracy.
+        </SpecialityListItem>
+        <SpecialityListItem>
+          <span>15.07.2020</span> – Witamy w naszych szeregach nowego
+          specjalistę ds. sprzedaży. Życzymy Tomkowi dobrych wyników i miłej
+          pracy.
+        </SpecialityListItem>
+        <SpecialityListItem>
+          <span>22.04.2020 </span>– Rozpoczynamy użytkowanie wózka bocznego
+          STILL
+        </SpecialityListItem>
+        <SpecialityListItem>
+          <span>15.04.2020</span> – Z przyjemnością informujemy o tym, że
+          rozpoczęliśmy sprzedaż przyłbic ochronnych. Zapraszamy do
+          naszegosklepu internetowego: sklep.forty.com.pl
+        </SpecialityListItem>
+        <SpecialityListItem>
+          <span>15.02.2020</span> – Witamy w naszych szeregach nowego
+          konstruktora. Marcin – życzymy sukcesów w pracy.
+        </SpecialityListItem>
+      </SpecialityList>
+    </SectionWrapper>
+  )
+}
+
+export default News
--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -1,24 +1,12 @@
 import React from 'react'
-import styled from 'styled-components'
 import Layout from '../components/Layout'
-// import Image from "../components/image"
+import News from '../components/News'
 import SEO from '../components/Seo'
-
-const MainSection = styled.section`
-  display: flex;
-`
-const MainTextWrapper = styled.div`
-  width: 55%;
-`
-const ImageWrapper = styled.div`
-  width: 45%;
-  max-width: 300px;
-  margin-bottom: 1.45rem;
-`
 
 const AboutUs: React.FC = () => (
   <Layout>
     <SEO title="O nas" />
+    <News />
   </Layout>
 )
 
